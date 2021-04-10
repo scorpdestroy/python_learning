1c066dc361579ea4535331716adee8b2ef63a350
scorpdestroy tolodo <shumilin.andrej@gmail.com>
2021-04-10 19:55:30 +0300

    data

--- Project1/выделение_данных.py
+++ Project1/выделение_данных.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import shutil
 
 directory = "C:/Users/shumi/Desktop/python_learning/python_tutor_htmls/"
 files = os.listdir(directory)
@@ -19,11 +20,21 @@ for x in files:
     items = soup.find_all('pre', class_='output')
     items_1 = soup.find_all('pre', class_='input')
 
+    os.mkdir("c://Users/shumi/Desktop/python_learning/Project1/MANY_THINGS/" + str(x))
+    
+    full_way_1 = "c://Users/shumi/Desktop/python_learning/Project1/MANY_THINGS/" + str(x)
+
     for item in items:
         print(item.getText())
         print("===")
+        f1 = open("output.txt", 'w')
+        f1.write(str(item.getText()))
+        f1.close()     
 
     for item_1 in items_1:
         print(item_1.getText())
         print("===")
-    break
+        f2 = open("input.txt", 'w')
+        f2.write(str(item_1.getText()))
+        f2.close()
+
